8fed6ad8534b7b681deef10d698add51f0cf01aa
ryujiin <ryujiin22@gmail.com>
2015-03-08 08:57:32 -0500

    comenzando domingo

--- oficina/static/js/backbone/views/catalogo/formulario_producto.js
+++ oficina/static/js/backbone/views/catalogo/formulario_producto.js
@@ -2,6 +2,7 @@ Oficina.Views.Formulario_producto = Backbone.View.extend({
 	el:$('#main'),
 	template : swig.compile($("#formulario_producto_template").html()),
 	events: {
+		'click .guardar_nuevo_color': 'add_color',
 	},
 	initialize : function () {
 		var self = this;
@@ -17,7 +18,15 @@ Oficina.Views.Formulario_producto = Backbone.View.extend({
 			this.llenar_color();
 		}
 	},
+	add_color:function () {
+		var nuevo_color = new Oficina.Models.Color();
+		var nuevo_nombre = this.$('#nuevo_color').val();
+		nuevo_color.set({nombre:nuevo_nombre});
+		nuevo_color.save();
+		this.$('#add_color_formulario').modal('hide')
+	},
 	llenar_color:function () {
+		this.$('#colores_formulario').empty();
 		var v_colores = new Oficina.Views.Colores({
 			collection : new Oficina.Collections.Colores()
 		});
