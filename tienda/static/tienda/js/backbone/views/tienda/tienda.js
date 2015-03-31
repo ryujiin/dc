8684752e4b70559487b79fa5c92e2d1c248af921
ryujiin <ryujiin22@gmail.com>
2015-03-31 18:05:10 -0500

    el menu de navegacion funciona en movil

--- tienda/static/tienda/js/backbone/views/tienda/tienda.js
+++ tienda/static/tienda/js/backbone/views/tienda/tienda.js
@@ -2,6 +2,8 @@ Loviz.Views.Tienda = Backbone.View.extend({
 	el:$('body'),
 	events: {
 		'click .link': 'link_intero',
+		'click header .menu_movil': 'desplegar_menu',
+		'click header .desplegado': 'ocultar_menu',
 	},
 	initialize : function () {
 	},
@@ -11,4 +13,14 @@ Loviz.Views.Tienda = Backbone.View.extend({
 		
 		window.routers.base.navigate(link, {trigger:true});
 	},
+	desplegar_menu:function () {
+		$('.main_menu').slideDown('slow');
+		$('.menu_movil').addClass('desplegado');
+		$('.menu_movil .icono').removeClass('icon-menu').addClass('icon-cross');
+	},
+	ocultar_menu:function () {
+		$('.main_menu').slideUp('slow');
+		$('.menu_movil').removeClass('desplegado');
+		$('.menu_movil .icono').removeClass('icon-cross').addClass('icon-menu');
+	}
 });
