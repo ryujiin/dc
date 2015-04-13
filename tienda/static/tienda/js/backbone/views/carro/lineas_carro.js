9c105094fc9a5ab8fe3d99abf628f35a96001610
ryujiin <ryujiin22@gmail.com>
2015-04-12 21:02:37 -0500

    las lineas suman y restan

--- tienda/static/tienda/js/backbone/views/carro/lineas_carro.js
+++ tienda/static/tienda/js/backbone/views/carro/lineas_carro.js
@@ -1,14 +1,15 @@
 Loviz.Views.Linea_carro = Backbone.View.extend({
     className: 'linea',
     events: {
+        'click .mas_produ': 'mas_productos',
+        'click .menos_produ': 'menos_productos',
     },
     template: swig.compile($("#lineas_carro_template").html()),
     
     initialize: function () {
         this.quick =false;
         this.listenTo(this.model, "change", this.render, this);        
-    },
-    
+    },    
     render: function () {
         var album = this.model.toJSON()
         var html = this.template(album);
@@ -21,15 +22,6 @@ Loviz.Views.Linea_carro = Backbone.View.extend({
             window.models.carro.fetch();
         });
     },
-    aumentar_cantidad:function () {
-        this.$('.up').hide();
-        var numero = parseInt(this.$('.cantidad_num').val());
-        numero++
-        this.model.set('cantidad',numero)
-        this.model.save().done(function () {
-            window.models.carro.fetch();
-        })
-    },
     reducir_cantidad:function () {
         this.$('.down').hide();        
         var numero = parseInt(this.$('.cantidad_num').val());
@@ -40,5 +32,19 @@ Loviz.Views.Linea_carro = Backbone.View.extend({
                 window.models.carro.fetch();
             })
         };
+    },
+    mas_productos:function () {
+        var cantidad = this.model.toJSON().cantidad;
+        this.model.set('cantidad',cantidad+1)
+        this.model.save().done(function () {
+            window.models.carro.fetch()
+        })
+    },
+    menos_productos:function () {
+        var cantidad = this.model.toJSON().cantidad;
+        this.model.set('cantidad',cantidad-1)
+        this.model.save().done(function () {
+            window.models.carro.fetch()
+        })
     }
 });
